9ac087cb15f8e0aa72ae6af6f69e4f20b074749c
Er. P. Rohit V. Acharya <61237636+rohitpidishetty@users.noreply.github.com>
2026-06-26 23:55:16 -0500

    Add files via upload

---
+++ com/fs/FileSegregator.java
@@ -0,0 +1,131 @@
+package com.fs;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileOutputStream;
+import java.nio.file.Files;
+import java.util.HashSet;
+import java.util.Set;
+
+public class FileSegregator {
+
+  private Set<String> fileExtensions = new HashSet<>();
+
+  public void scanFileTypes(String filePath, Set<String> feObj) {
+    File file = new File(filePath);
+    if (!file.exists()) {
+      System.out.println("[ERROR] File does not exists");
+      System.exit(1);
+    }
+    if (file.isFile()) {
+      String fName = file.getName();
+      if (!fName.contains(".")) return;
+      feObj.add(fName.split("[.]")[1]);
+      return;
+    }
+    for (File subFile : file.listFiles())
+      scanFileTypes(subFile.getAbsolutePath(), feObj);
+  }
+
+  public void createDirectories(String rootFilePath) throws Exception {
+    File file = new File(rootFilePath);
+    if (!file.exists()) file.mkdir();
+    this.fileExtensions.iterator().forEachRemaining(e -> {
+      try {
+        new File(file.getAbsoluteFile(), e.concat("_files")).mkdir();
+      } catch (Exception err) {
+        System.out.println(err);
+      }
+    });
+    new File(file.getAbsoluteFile(), "extra_files").mkdir();
+  }
+
+  public void copyAllFilesIntoSegregatedDirectories(
+    String filePath,
+    String newRootFilePath
+  ) {
+    File file = new File(filePath);
+    if (file.isFile()) {
+      String fName = file.getName();
+      String extension = fName.contains(".") ? fName.split("[.]")[1] : "extra";
+      String copyTo = newRootFilePath
+        .concat("\\")
+        .concat(extension)
+        .concat("_files\\")
+        .concat(fName);
+      System.out.printf(
+        "[INFO] Copying file contents from [%s] to [%s]%n",
+        file.getAbsolutePath(),
+        copyTo
+      );
+      File f = new File(copyTo);
+
+      if (!f.exists()) {
+        try {
+          f.createNewFile();
+          System.out.println("Created");
+        } catch (Exception e) {}
+      }
+
+      try (
+        FileInputStream fis = new FileInputStream(file);
+        FileOutputStream fos = new FileOutputStream(new File(copyTo));
+      ) {
+        byte[] byteStream = fis.readAllBytes();
+        fos.write(byteStream);
+      } catch (Exception e) {
+        System.out.println(e.getMessage());
+        System.exit(1);
+      }
+      try {
+        Files.deleteIfExists(file.toPath());
+      } catch (Exception e) {
+        System.out.println(e.getMessage());
+        System.exit(1);
+      }
+      return;
+    }
+    for (File subFile : file.listFiles()) {
+      copyAllFilesIntoSegregatedDirectories(
+        subFile.getAbsolutePath(),
+        newRootFilePath
+      );
+    }
+  }
+
+  public void segregate(String from, String to) {
+    try {
+      FileSegregator fl = new FileSegregator();
+      fl.fileExtensions = new HashSet<>();
+      fl.scanFileTypes(from, fl.fileExtensions);
+      System.out.println("[INFO] Starting file system scan...");
+      Thread t1 = new Thread(() -> {
+        try {
+          Thread.sleep(1000);
+          System.out.println("[INFO] Discovering supported file types...");
+
+          Thread.sleep(1000);
+          System.out.println(
+            "[INFO] File type discovery completed successfully."
+          );
+          System.out.println(
+            "[INFO] Supported file types: " + fl.fileExtensions
+          );
+        } catch (InterruptedException e) {
+          Thread.currentThread().interrupt();
+          System.err.println("[ERROR] File type discovery was interrupted.");
+        }
+      });
+      t1.start();
+      fl.createDirectories(to);
+      t1.join();
+      Thread.sleep(200);
+      System.out.println("[INFO] Successfully created all sub-directories");
+      fl.copyAllFilesIntoSegregatedDirectories(from, to);
+      System.out.println("[INFO] Segregation Completed");
+    } catch (Exception e) {
+      System.out.println(e.getMessage());
+    }
+  }
+}
---
+++ com/fs/FileSystem.java
@@ -0,0 +1,155 @@
+package com.fs;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class FileSystem {
+
+  private static double globalFileSize = 0;
+
+  private static void getFileSize(
+    String filePath,
+    File file,
+    boolean status,
+    String unit,
+    String type
+  ) {
+    try {
+      double size = (Files.readAllBytes(Paths.get(filePath)).length);
+      if (status) printSizeOnScreen(size, file, unit, type);
+      globalFileSize += size;
+    } catch (Exception e) {
+      System.out.println(e.getMessage());
+    }
+  }
+
+  private static void printSizeOnScreen(
+    double size,
+    File file,
+    String unit,
+    String type
+  ) {
+    String _unit_ = new String("B");
+    if (unit.equals("-kb")) {
+      size /= 1024d;
+      _unit_ = "KB";
+    } else if (unit.equals("-mb")) {
+      size /= (1024d * 1024d);
+      _unit_ = "MB";
+    } else if (unit.equals("-gb")) {
+      size /= (1024d * 1024d * 1024d);
+      _unit_ = "GB";
+    }
+    System.out.printf(
+      "%s: %-30s | SIZE: %8.6f%s%n",
+      type,
+      file.getName(),
+      size,
+      _unit_
+    );
+  }
+
+  private static void dfs(String folderPath, File file, String unit) {
+    if (file.isFile()) {
+      getFileSize(folderPath, file, false, unit, "FILE");
+      return;
+    }
+    for (File subFile : file.listFiles())
+      dfs(subFile.getAbsolutePath(), subFile, unit);
+  }
+
+  public static void main(String[] args) {
+    if (args.length == 0) {
+      printUsage();
+      System.exit(1);
+    }
+
+    String command = args[0];
+
+    try {
+      switch (command) {
+        case "-size":
+          if (args.length != 3) {
+            System.err.println("Error: Invalid arguments for -size command.");
+            printUsage();
+            System.exit(1);
+          }
+
+          String path = args[1];
+          String unit = args[2];
+
+          if (
+            !unit.equals("-kb") && !unit.equals("-mb") && !unit.equals("-gb")
+          ) {
+            System.err.println(
+              "Error: Invalid size unit. Use -kb, -mb, or -gb."
+            );
+            System.exit(1);
+          }
+
+          File file = new File(path);
+
+          if (!file.exists()) {
+            System.err.println("Error: Path does not exist: " + path);
+            System.exit(1);
+          }
+
+          if (file.isFile()) {
+            getFileSize(path, file, true, unit, "FILE");
+          } else if (file.isDirectory()) {
+            dfs(path, file, unit);
+            printSizeOnScreen(globalFileSize, file, unit, "FOLDER");
+          } else {
+            System.err.println("Error: Unsupported path type: " + path);
+            System.exit(1);
+          }
+          break;
+        case "-seg":
+          if (args.length != 3) {
+            System.err.println("Error: Invalid arguments for -seg command.");
+            printUsage();
+            System.exit(1);
+          }
+
+          String sourcePath = args[1];
+          String destinationPath = args[2];
+
+          File source = new File(sourcePath);
+
+          if (!source.exists()) {
+            System.err.println(
+              "Error: Source path does not exist: " + sourcePath
+            );
+            System.exit(1);
+          }
+
+          new FileSegregator().segregate(sourcePath, destinationPath);
+          break;
+        default:
+          System.err.println("Error: Unknown command: " + command);
+          printUsage();
+          System.exit(1);
+      }
+    } catch (Exception e) {
+      System.err.println("Error: Operation failed.");
+      System.err.println("Reason: " + e.getMessage());
+      System.exit(1);
+    }
+  }
+
+  private static void printUsage() {
+    System.err.println(
+      """
+      Usage:
+          java FileSystem -size <file-path> <-kb|-mb|-gb>
+          java FileSystem -seg <file-path-from> <file-path-to>
+
+      Examples:
+          java FileSystem -size "C:\\Users\\Amani\\Desktop\\test.txt" -mb
+          java FileSystem -size "C:\\Users\\Amani\\Desktop" -gb
+          java FileSystem -seg "C:\\Users\\Amani\\Downloads" "C:\\Users\\Amani\\SortedFiles"
+      """
+    );
+  }
+}
