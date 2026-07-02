c90fb21639d84249d161cf3db36f60958bd6e3a2
Er. P. Rohit V. Acharya <61237636+rohitpidishetty@users.noreply.github.com>
2026-07-01 22:36:56 -0500

    Add files via upload

---
+++ com/fs/FileCreator.java
@@ -0,0 +1,10 @@
+// package com.fs;
+
+public class FileCreator {
+
+  public static void main(String[] args) {
+    // java -jar FileKit.jar -create <file-name> <destination-directory>
+    String fileName = "x.txt";
+    String destination = "C:\\Users\\rohit\\Desktop";
+  }
+}
